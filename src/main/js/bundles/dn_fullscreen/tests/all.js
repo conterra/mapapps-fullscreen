8b96c9642baa594313a0811dad6a1738c5c4bcef
Matthias Stein <m.stein@conterra.de>
2021-01-27 17:54:39 +0100

    Increase inception year

--- src/main/js/bundles/dn_fullscreen/tests/all.js
+++ src/main/js/bundles/dn_fullscreen/tests/all.js
@@ -13,9 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module.exports = {
-    root: {
-        apptitle: "Fullscreen-Embedded"
-    },
-    "de": true
-};
