0189480b95b5e261c8f30a2591f8e6e1e5c22db7
Nick Jakuschona <n_jaku01@wwu.de>
2022-12-01 11:21:01 +0100

    rename fullscreen css class

--- src/main/js/bundles/dn_fullscreen/FullscreenController.js
+++ src/main/js/bundles/dn_fullscreen/FullscreenController.js
@@ -33,7 +33,7 @@ export default class {
         tool.set("iconClass", "esri-icon-zoom-in-fixed");
         const root = this._appCtx.getApplicationRootNode();
         const properties = this._properties;
-        const mapDiv = (properties.onlyMap) ? root.getElementsByClassName("mainMap")[0] : root;
+        const mapDiv = (properties.onlyMap) ? root.getElementsByClassName("ct-application-center")[0] : root;
         if(mapDiv) {
             if (mapDiv.requestFullscreen) {
                 mapDiv.requestFullscreen();
@@ -69,7 +69,7 @@ export default class {
             if (document.exitFullscreen) {
                 document.exitFullscreen();
                 if (properties.hideOmnisearch) {
-                    showOmnisearch(root)
+                    showOmnisearch(root);
                 }
             } else if (document.webkitExitFullscreen) { /* Safari */
                 document.webkitExitFullscreen();
